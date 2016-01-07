1128a66d75cc5d50dc332eccba62185562ec8fa7
FGtatsuro <fujiistorage@gmail.com>
2016-01-07 15:34:06 +0900

    Commit for demo.

---
+++ js/init.js
@@ -0,0 +1,11 @@
+﻿/* 初期化処理 */
+$(document).ready(function() {
+    // 他のKii SDKが実行される前に、
+    // AppID, AppKey, SiteID
+    // で初期化します。
+    Kii.initializeWithSite("3cee7f37", "3970e87e29d5f1187344761e365dd20c", KiiSite.JP);
+    console.log("Kii.initializeWithSite");
+
+    // アクセストークンの有効期限の設定（単位：秒）
+    Kii.setAccessTokenExpiration(3600);
+});
