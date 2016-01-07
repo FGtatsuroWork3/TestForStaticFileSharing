2123ab6457551bef4724c6a68aebb57cd0a9e9f9
FGtatsuro <fujiistorage@gmail.com>
2016-01-07 15:46:44 +0900

    Fix initialization.

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // 他のKii SDKが実行される前に、
     // AppID, AppKey, SiteID
     // で初期化します。
-    Kii.initializeWithSite("3cee7f37", "3970e87e29d5f1187344761e365dd20c", KiiSite.JP);
+    Kii.initializeWithSite("d85f38e3", "8fb292f2054bb88ae0390ced57e65b81", KiiSite.JP);
     console.log("Kii.initializeWithSite");
 
     // アクセストークンの有効期限の設定（単位：秒）
